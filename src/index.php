d8b82822678d76167ffdf49482d83fbe1c435a29
caiyanhu <caiyanhu752925240@qq.com>
2020-03-25 09:21:59 +0800

    feat: 变成13位时间戳

--- src/index.php
+++ src/index.php
@@ -4,16 +4,16 @@ date_default_timezone_set("PRC");
 $query = empty($argv[1]) ? '' : $argv[1];
 
 if (empty($query)) {
-	$param = time();
+	$param = microtime();
 	$output = '当前时间戳为：' . $param;
 }
 else if (is_numeric($query)) {
 	$param = date('Y-m-d H:i:s', $query);
-	$output = $param;
+	$output = $param+"000";
 }
 else {
 	$param = strtotime($query);
-	$output = $param;
+	$output = $param+"000";
 }
 
 echo '<?xml version="1.0" encoding="utf-8"?>
